edf5f3b840fc03c2f09f4c970f694b5ef04b2dec
sivaru <sergioivu@gmail.com>
2017-06-07 14:52:56 -0600

    Propuesta de herencia

--- formulario.sql
+++ formulario.sql
@@ -1,5 +1,16 @@
 create type formulario as object(
 	codigo int,
 	hidrante GeoPoint,
+)not final;
+/
+
+create type mantenimiento under formulario(
+	tipo int
 );
 /
+
+create type realizado under formulario(
+	anotacion varchar(100)
+);
+/
+
