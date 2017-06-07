ef219d230b68e643613438b040e9a87037dd53c1
jcarbad <jcarbad@gmail.com>
2017-06-07 17:48:42 -0600

    Metodo calcular distancia_a(GeoPoint)

--- Script Maestro.sql
+++ Script Maestro.sql
@@ -1,7 +1,7 @@
 -- Constantes
 DECLARE 
    PI constant number := 3.141592654;
-   RADIO_TIERRA_KM INT := 6371; 
+   
 BEGIN  
    -- output 
    dbms_output.put_line('Pi: ' || PI); 
@@ -33,7 +33,17 @@ create or replace type GeoPoint as object
 );
 /
 
--- Objeto Hidrante
+--funciones convertir Grados a Radianes
+create or replace function degreesToRadians(degrees float)
+return float
+is 
+	radian float;
+begin
+	radian := (degrees * 3.1415926535)/180;
+	return radian;
+end;
+/
+
 create or replace type Hidrante as object(
 	id INTEGER,
 	posicion GeoPoint,
@@ -48,22 +58,47 @@ create or replace type Hidrante as object(
 );
 /
 
-CREATE OR REPLACE TYPE BODY Hidrante AS 
-	MEMBER FUNCTION distancia_a(punto GeoPoint) RETURN FLOAT IS
+CREATE OR REPLACE TYPE BODY Hidrante AS
+	-- Haversine Formula 
+	MEMBER FUNCTION distancia_a(punto GeoPoint) RETURN FLOAT
+	IS
+		-- 1 Hidante, 2 Punto
+		RADIO_TIERRA_KM constant INT := 6371; 
+		gLatHidrante FLOAT := posicion.latitud;
+		gLonHidrante FLOAT := posicion.longitud;
+		gLatPunto FLOAT := punto.latitud;
+		gLonPunto FLOAT := punto.longitud;
+		deltaLat FLOAT;
+		deltaLon FLOAT;
+		radLatHidrante FLOAT;
+		radLatPunto FLOAT;
+		a FLOAT;
+		c FLOAT;
+		d FLOAT;
 	BEGIN
-		RETURN 0;
+		deltaLat := degreesToRadians(gLatPunto - gLatHidrante);
+		deltaLon := degreesToRadians(gLonPunto - gLonHidrante);
+
+		radLatHidrante := degreesToRadians(gLatHidrante);
+		radLatPunto := degreesToRadians(gLatPunto);
+
+		a := SIN(deltaLat / 2) * SIN(deltaLat / 2) + SIN(deltaLon / 2) * SIN(deltaLon / 2) * COS(radLatHidrante) * COS(radLatPunto);
+		c := 2 * ATAN2(SQRT(a), SQRT(1-a));
+		d := RADIO_TIERRA_KM * c;
+		RETURN d;
 	END distancia_a;
 END;
 /
 
+CREATE OR REPLACE PROCEDURE pruebaHidrantes 
+AS
+	nuevo Hidrante := Hidrante(1, GeoPoint(9.9721549,-84.1283363), 10, 7, 50.3, 1, bombero('1', 'Sergio'), SYSDATE,0);
+	camion GeoPoint := GeoPoint(9.9983516,-84.139515);
+	distancia FLOAT;
+BEGIN
+	distancia := nuevo.distancia_a(camion);
+	dbms_output.put_line('SE ENCUENTRA A KM: ' || distancia);
+END;
+/ 
 
---funciones almacenadas
-create or replace function degreestoradians(degrees float)
-return float
-is 
-	radian float;
-begin
-	radian := (degrees * 3.1415926535)/180;
-	return radian;
-end;
-/
+
