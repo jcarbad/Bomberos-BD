f80b768e10a41299acb50618930d817140b174bb
jcarbad <jcarbad@gmail.com>
2017-06-07 18:15:23 -0600

    Ignorar este commit

--- PruebaMetodoCoords.js
+++ PruebaMetodoCoords.js
@@ -6,16 +6,15 @@ function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
   var earthRadiusKm = 6371;
 
   var dLat = degreesToRadians(lat2-lat1).toFixed(10);
-  console.log(dLat);
+
   var dLon = degreesToRadians(lon2-lon1).toFixed(10);
-  console.log(dLon);
+  
   lat1 = degreesToRadians(lat1).toFixed(10);
   lat2 = degreesToRadians(lat2).toFixed(10);
-  console.log(lat1);
-  console.log(lat2);
+
   var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
           Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-  console.log(a); 
+
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
   return earthRadiusKm * c;
 }
